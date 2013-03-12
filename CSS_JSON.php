f514266f7ba0c37106a733c8310523ff7624f389
David McClure <david.mcclure@virginia.edu>
2013-03-12 15:56:20 -0400

    Basic shell for class.

---
+++ CSS_JSON.php
@@ -0,0 +1,42 @@
+<?php
+
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4 cc=76; */
+
+/**
+ * Convert CSS to JSON, JSON to CSS.
+ *
+ * @package     omeka
+ * @subpackage  neatline
+ * @copyright   2012 Rector and Board of Visitors, University of Virginia
+ * @license     http://www.apache.org/licenses/LICENSE-2.0.html
+ */
+
+
+class CSS_JSON {
+
+
+    /**
+     * Convert CSS to JSON.
+     *
+     * @param string $css CSS.
+     * @return string $json JSON.
+     */
+    public static function cssToJson($css)
+    {
+
+    }
+
+
+    /**
+     * Convert JSON to CSS.
+     *
+     * @param string $json JSON.
+     * @return string $css CSS.
+     */
+    public static function jsonToCss($json)
+    {
+
+    }
+
+
+}
